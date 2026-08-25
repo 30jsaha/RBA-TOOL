4ab4731293810442e83cfbf397cd73bce399a982
Jyotirmoy <30jsaha@gmail.com>
2026-08-25 16:14:37 +0530

    removed rbafront folder

--- frontend/rbafront/src/components/layout/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import "./css/Footer.css";
-export default function Footer() {
-  return (
-    <footer className="text-light text-center py-3 mt-auto">
-      <small className="footer-text">© {new Date().getFullYear()} RBA Tool. All rights reserved.</small>
-    </footer>
-  );
-}
